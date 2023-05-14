e6c4d54f912b7b0e3f1cf9103ffcf562d35028a5
Petr Persianov <persianov.petr@gmail.com>
2023-05-15 00:43:25 +0200

    Added new kraken taxonomy reports. Updated features. Added randomized search for RF.

--- kraken_taxonomy/extract_taxonomy_from_kraken_report.py
+++ kraken_taxonomy/extract_taxonomy_from_kraken_report.py
@@ -6,13 +6,22 @@ from multiprocessing import freeze_support
 # define a function to process each accession
 def process_accession(acc):
     try:
-        df = pd.read_table(f"./data/{acc}/report.txt",names=["%", "total count", "self count", "taxonomy", "tax id", "name"], skipinitialspace=True)
-        d_index = list(filter(lambda x: x > df.index[df['name'] == 'Bacteria'].to_list()[0], df.index[df['taxonomy'] == 'D']))
+        df = pd.read_table(
+            f"./data/{acc}/report.txt",
+            names=["%", "total count", "self count", "taxonomy", "tax id", "name"],
+            skipinitialspace=True,
+        )
+        d_index = list(
+            filter(
+                lambda x: x > df.index[df["name"] == "Bacteria"].to_list()[0],
+                df.index[df["taxonomy"] == "D"],
+            )
+        )
         if len(d_index) == 0:
             df = df.loc[::, ::]
         else:
-            df = df.loc[:d_index[0] - 1, ::]
-        df = df.loc[(df['taxonomy'] == "S")]
+            df = df.loc[: d_index[0] - 1, ::]
+        df = df.loc[(df["taxonomy"] == "S")]
         df = df[["total count", "name"]].set_index("name").T
         df.insert(0, "Sample", str(acc))
         if df.shape[1] > 2000:
@@ -21,22 +30,25 @@ def process_accession(acc):
             print(f"BAD accession: {acc}")
             return None
     except FileNotFoundError as error:
-    #   print(f"Report not found for {acc}", error)
-      return None
+        print(f"Report not found for {acc}", error)
+        return None
 
-if __name__ == '__main__':
-  freeze_support()
-  # read and process accessions concurrently
-  metadata = pd.read_csv("../metadata.csv", sep=";")
-  accessions = metadata[["Run"]]["Run"].to_list()
 
-  result = pd.concat([
-        df
-        for df in concurrent.futures.ProcessPoolExecutor().map(
-            process_accession, accessions
-        )
-        if df is not None
-    ])
-  result = result.fillna(0)
-  result = result.set_index("Sample", drop=True)
-  result.to_csv("features.csv", sep=";")
+if __name__ == "__main__":
+    freeze_support()
+    # read and process accessions concurrently
+    metadata = pd.read_csv("../metadata.csv", sep=";")
+    accessions = metadata[["Run"]]["Run"].to_list()
+
+    result = pd.concat(
+        [
+            df
+            for df in concurrent.futures.ProcessPoolExecutor().map(
+                process_accession, accessions
+            )
+            if df is not None
+        ]
+    )
+    result = result.fillna(0)
+    result = result.set_index("Sample", drop=True)
+    result.to_csv("features.csv", sep=";")
