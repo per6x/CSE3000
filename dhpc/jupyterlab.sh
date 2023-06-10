79324faf0588fe7b5644cc6d2f8368f678dc3bee
Petr Persianov <persianov.petr@gmail.com>
2023-06-10 15:40:57 +0200

    Updated jupyterlab dhpc script

--- dhpc/jupyterlab.sh
+++ dhpc/jupyterlab.sh
@@ -2,27 +2,23 @@
 
 #SBATCH --job-name=jupyter
 #SBATCH --partition=compute
-#SBATCH --time=05:00:00
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
 #SBATCH --account=education-eemcs-courses-cse3000
 
 # Load modules:
-module load 2022r2
+module load 2023r1
 module load openmpi
 module load miniconda3
-module load python
-module load py-numpy py-pip py-scipy py-scikit-learn py-matplotlib
-
-
 
 # Set conda env:
 unset CONDA_SHLVL
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
-conda activate jupyterlab
+conda activate rp
 cat /etc/hosts
 jupyter lab --ip=0.0.0.0 --port=8888
 conda deactivate
